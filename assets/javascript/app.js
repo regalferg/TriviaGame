ca01fd2b9033e04cac97a17875590f79b5b93a48
Charles <regalferg@gmail.com>
2017-12-13 10:00:59 -0500

    Changes to colors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,28 @@
 var correctAnswer = 0;
 var incorrectAnswer = 0;
 var noAnswer = 0;
+var time = 0;
 
 
 
 $("#start-button").on("click", function(){
 $(".wrapper").css("display", "none");
 $("form").css("display", "block");
+    // time =  setTimeout(30000);
+    // var displayTime = timeConverter(time);
+
+    // $("#display").html(displayTime);
+
+       setTimeout(function(){
+        fiveSeconds();
+      },5000);
+          function fiveSeconds() {
+            console.log("5 Seconds Left!!!");
+
+        $("#display").html("<h2> 5 SECONDS LEFT</h2>");
+        
+      }
+
 
 
 });
@@ -44,3 +60,25 @@ if ($("#optionRadio3").prop('checked') == false) {
 
 
 });
+
+
+  timeConverter =  function(t) {
+
+    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
