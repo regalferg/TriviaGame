3ba45301dfd147e34812f213c4ad84320d1343b6
Charles <regalferg@gmail.com>
2017-12-14 00:11:56 -0500

    2nd Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,84 +1,90 @@
 var correctAnswer = 0;
 var incorrectAnswer = 0;
 var noAnswer = 0;
-var time = 0;
+var time = 10;
+var intervalId;
 
 
+$(document).ready(function(){
 
+// GAME START
 $("#start-button").on("click", function(){
 $(".wrapper").css("display", "none");
 $("form").css("display", "block");
-    // time =  setTimeout(30000);
-    // var displayTime = timeConverter(time);
-
-    // $("#display").html(displayTime);
-
-       setTimeout(function(){
-        fiveSeconds();
-      },5000);
-          function fiveSeconds() {
-            console.log("5 Seconds Left!!!");
-
-        $("#display").html("<h2> 5 SECONDS LEFT</h2>");
-        
-      }
-
-
+run();
 
 });
 
+
+
+
+// Submit Button FORM
 $("#submit-button").on("click", function(){
 $("form").css("display", "none");
 $(".wrapper2").css("display", "block");
-
+triviaStart();
 });
 
-$("#triviaForm input").on("change",function(){
+function triviaStart(){
+  $("form").css("display", "none");
+$(".wrapper2").css("display", "block");
+var answers =[
+$('input[name=optionRadio1]:checked').val(),
+$('input[name=optionRadio2]:checked').val(),
+$('input[name=optionRadio3]:checked').val(),
+];
+for (var i = 0; i < answers.length; i++) {
+  console.log(answers[i]);
+  
+  if (answers[i] == 7) {
+    correctAnswer++;
+    $("#correct").html(correctAnswer);
 
+  }
+   if (answers[i] == 0) {
+    incorrectAnswer++;
+    $("#incorrect").html(incorrectAnswer);
 
-	if ($('input[type=radio]:checked').val() == 7) {
+}
+else  {
+   noAnswer++;
+    $("#noanswer").html(noAnswer);
 
-correctAnswer++;
-$("#correct").html(correctAnswer);
-
-
-};
- if($('input[type=radio]:checked', '#triviaForm').val() == 0){
-		incorrectAnswer++;
-	$("#incorrect").html(incorrectAnswer);
-	}
-	
-
-if ($("#optionRadio3").prop('checked') == false) {
-
-			noAnswer++;
-	$("#noanswer").html(noAnswer);
-	console.log("no answer");
+}
 }
 
 
 
+}
 
+
+
+function run() {
+ 
+      intervalId = setInterval(decrement, 1000);
+
+}
+function decrement() {
+      time--;
+
+      $("#display").html("<h2>" + time + "</h2>");
+      if (time == 0) {
+        clearInterval(intervalId);
+        triviaStart();
+
+      }
+
+    }
+  
+
+
+
+
+
+
+
+
+
+
+// END Doc Ready
 });
-
-
-  timeConverter =  function(t) {
-
-    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-
-    if (minutes === 0) {
-      minutes = "00";
-    }
-
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-
-    return minutes + ":" + seconds;
-  }
